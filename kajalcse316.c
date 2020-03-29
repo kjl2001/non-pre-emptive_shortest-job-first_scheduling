16d42f623d5ee6610a4bddbb2dc874b2aac4ae5e
kjl2001 <62831050+kjl2001@users.noreply.github.com>
2020-03-29 16:21:46 +0530

    Add files via upload

---
+++ kajalcse316.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\t\t\t----------------------- Scheduling -----------------------\n\n\n\n");
+  long int n,i=0,j=0;
+  printf("Enter Number of Processes : ");
+  scanf("%ld",&n ); 
+  double priority[n],avg_waiting,avg_turnaround,burstTime[n],arrivalTime[n],waitingTime[n],turnaroundTime[n], process[n], temp, completionTime[n],min,sum=0,sum2=0,wait_final, turnaround_final, wait_avg, turnaround_avg;
+  for(i=0;i<n;i++)
+  {
+    printf("\nEnter Burst Time for Process [%d] : ", i+1 );
+    scanf("%lf", &burstTime[i]);
+    printf("Enter Arrival Time for Process [%d] : ", i+1 );
+    scanf("%lf", &arrivalTime[i] );
+    process[i]=i+1;
+  }
+
+  printf("\n\n\t\t\t -------------- Entered Values are --------------\n\n");
+  printf("\t\t\t---------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
+  printf("\t\t\t---------------------------------------\n");
+  for(i=0;i<n;i++)
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |\n",process[i],arrivalTime[i],burstTime[i]);
+  }
+    printf("\t\t\t---------------------------------------\n");
+
+
+  printf("\n\n\t\t\t-------- Sorting Processes according to Arrivaltime --------\n");
+	
+  getch();
+  return 0;
+}
+
